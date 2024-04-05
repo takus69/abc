89390bb7c3497a243329235e80abf1616cce9e52
Takuya Saito <takus4649@gmail.com>
2024-04-05 20:52:18 +0900

    ABC064 D

---
+++ src/bin/abc064_d.rs
@@ -0,0 +1,37 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: i32,
+        s: Chars,
+    }
+    let mut l_cnt: i32 = 0;
+    let mut l_cnt_max: i32 = 0;
+    let mut r_cnt: i32 = 0;
+    let mut r_cnt_max: i32 = 0;
+    for ss in &s {
+        if ss == &')' {
+            l_cnt += 1;
+            l_cnt_max = l_cnt_max.max(l_cnt);
+        } else {
+            l_cnt -= 1;
+        }
+    }
+    if l_cnt < 0 {
+        l_cnt = 0;
+    }
+    for ss in s.iter().rev() {
+        if ss == &'(' {
+            r_cnt += 1;
+            r_cnt_max = r_cnt_max.max(r_cnt);
+        } else {
+            r_cnt -= 1;
+        }
+    }
+    if r_cnt < 0 {
+        r_cnt = 0;
+    }
+    println!("{}", '('.to_string().repeat(l_cnt_max as usize) + &s.iter().collect::<String>() + &')'.to_string().repeat(r_cnt_max as usize));
+
+}
