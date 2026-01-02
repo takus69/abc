4e084efa37de1d941ae585628e3b1e9b68653446
takus <takus4649@gmail.com>
2026-01-03 05:22:22 +0900

    tessoku A49 beam search

--- src/bin/tessoku_a49.rs
+++ src/bin/tessoku_a49.rs
@@ -1,5 +1,51 @@
 use proconio::input;
 use itertools::Itertools;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct State {
+    score: usize,
+    ans: Vec<char>,
+    a: Vec<isize>,
+}
+
+impl State {
+    fn new() -> Self {
+        let score = 0;
+        let ans: Vec<char> = Vec::new();
+        let a: Vec<isize> = vec![0; 20];
+
+        Self { score, ans, a }
+    }
+
+    fn cmd(&mut self, c: char, p: usize, q: usize, r: usize) {
+        self.score += self.simulate(c, p, q, r);
+
+        let d = if c == 'A' { 1 } else { -1 };
+        for i in [p, q, r] {
+            self.a[i] += d;
+        }
+        self.ans.push(c);
+    }
+
+    fn simulate(&self, c: char, p: usize, q: usize, r: usize) -> usize {
+        let mut add_score = 0;
+        let d = if c == 'A' { 1 } else { -1 };
+        for i in [p, q, r] {
+            if self.a[i] == -d {
+                add_score += 1;
+            }
+        }
+        for i in 0..20 {
+            if self.a[i] == 0 && ![p,q,r].contains(&i) {
+                add_score += 1;
+            }
+        }
+
+        add_score
+    }
+}
 
 fn main() {
     input! {
@@ -11,27 +57,30 @@ fn main() {
         pqr[i].1 -= 1;
         pqr[i].2 -= 1;
     }
-    let mut x: Vec<isize> = vec![0; 20];
-    let mut score = 0;
-    let mut ans: Vec<char> = Vec::new();
-    for &(p, q, r) in pqr.iter() {
-        let mut a0 = 0;
-        for i in [p, q, r] {
-            if x[i] == 0 { a0 -= 1; } else if x[i] == -1 { a0 += 1; }
-        }
-        let mut b0 = 0;
-        for i in [p, q, r] {
-            if x[i] == 0 { b0 -= 1; } else if x[i] == 1 { b0 += 1; }
-        }
-        let d = if a0 > b0 { ans.push('A');1 } else { ans.push('B');-1 };
-        for i in [p, q, r] {
-            x[i] += d;
-        }
 
-        for i in 0..20 {
-            if x[i] == 0 { score += 1; }
+    let mut beams: BinaryHeap<Reverse<State>> = BinaryHeap::new();
+    let beam_width = 1000;
+
+    let state = State::new();
+    beams.push(Reverse(state));
+
+    for &(p, q, r) in pqr.iter() {
+        let mut next_beams: BinaryHeap<Reverse<State>> = BinaryHeap::new();
+        while let Some(Reverse(state)) = beams.pop() {
+            for c in ['A', 'B'] {
+                let mut next_state = state.clone();
+                next_state.cmd(c, p, q, r);
+                next_beams.push(Reverse(next_state));
+                if next_beams.len() > beam_width {
+                    next_beams.pop();
+                }
+            }
         }
+        beams = next_beams;
     }
-    eprintln!("score: {}", score);
-    println!("{}", ans.iter().join(" "));
+
+    let Reverse(state) = beams.iter().last().unwrap();
+
+    eprintln!("score: {}", state.score);
+    println!("{}", state.ans.iter().join(" "));
 }
