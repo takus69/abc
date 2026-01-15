5db52326de546637d2dd374b741b96bc1696f673
Takuya Saito <takus4649@gmail.com>
2026-01-16 05:21:03 +0900

    tessoku A67 use Dsu

--- src/bin/tessoku_a67.rs
+++ src/bin/tessoku_a67.rs
@@ -1,30 +1,19 @@
 use proconio::input;
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
+use ac_library::Dsu;
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        abc: [(usize, usize, usize); m],
+        mut abc: [(usize, usize, usize); m],
     }
-    let mut graph: Vec<Vec<(usize, usize)>> = vec![Vec::new(); n+1];
-    for &(a, b, c) in abc.iter() {
-        graph[a].push((b, c));
-        graph[b].push((a, c));
-    }
-    let mut heap: BinaryHeap<(Reverse<usize>, usize)> = BinaryHeap::new();
-    let mut fixed: Vec<bool> = vec![false; n+1];
+    abc.sort_by(|a, b| a.2.cmp(&b.2));
+    let mut dsu: Dsu = Dsu::new(n+1);
     let mut ans = 0;
-    heap.push((Reverse(0), 1));
-    while let Some((Reverse(c), i)) = heap.pop() {
-        if fixed[i] { continue; }
-        fixed[i] = true;
+    for &(a, b, c) in abc.iter() {
+        if dsu.same(a, b) { continue; }
         ans += c;
-        for &(j, c2) in graph[i].iter() {
-            if fixed[j] { continue; }
-            heap.push((Reverse(c2), j));
-        }
+        dsu.merge(a, b);
     }
 
     println!("{}", ans);
