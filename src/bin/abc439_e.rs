a38fbd24813565a39bbd0d94d154307426222c43
Takuya Saito <takus4649@gmail.com>
2026-03-26 07:43:27 +0900

    ABC439 E

--- src/bin/abc439_e.rs
+++ src/bin/abc439_e.rs
@@ -1,41 +1,23 @@
 use proconio::input;
+use std::cmp::Reverse;
 
 fn main() {
     input! {
         n: usize,
         mut ab: [(usize, usize); n],
     }
-    ab.sort_by(|a, b| a.0.max(a.1).cmp(&(b.0.max(b.1))));
-    // println!("ab: {:?}", ab);
-    let mut dp: Vec<Vec<usize>> = vec![vec![0, 0]];
-    let mut pre_a = (0, 0);
-    let mut pre_b = (0, 0);
-    for &(a, b) in ab.iter() {
-        let dpi = dp.last().unwrap();
-        let (mut dp0, mut dp1) = (dpi[0], dpi[1]);
-        let dp2 = (dp0+1).max(dp1+1);
-        if pre_a.0 < a && pre_a.1 < b {
-            if pre_b.0 < a && pre_b.1 < b {
-                dp0 = dp2;
-            } else {
-                dp0 += 1;
-            }
-            pre_a = (a, b);
-        } else if pre_a.0 > a {
-            pre_a = (a, b);
+    ab.sort_by(|a, b| (a.0, Reverse(a.1)).cmp(&(b.0, Reverse(b.1))));
+    // println!("{:?}", ab);
+    // aでソートすると、bの最長増加部分列を求めるだけ
+    let mut l: Vec<usize> = vec![usize::MAX];
+    for &(_, b) in &ab {
+        if l.last().unwrap() < &b  {
+            l.push(b);
+        } else {
+            let idx = l.partition_point(|&x| x < b);
+            l[idx] = b;
         }
-        if pre_b.0 < a && pre_b.1 < b {
-            if pre_a.0 < a && pre_a.1 < b {
-                dp1 = dp2;
-            } else {
-                dp1 += 1;
-            }
-            pre_b = (a, b);
-        } else if pre_b.1 > b {
-            pre_b = (a, b);
-        }
-        dp.push(vec![dp0, dp1]);
     }
-
-    println!("{}", dp[n][0].max(dp[n][1]));
+    // println!("{:?}", l);
+    println!("{}", l.len());
 }
