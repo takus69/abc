67a5e8d426689047a1680113a86d65a18097f0c5
Takuya Saito <takus4649@gmail.com>
2026-04-24 19:49:31 +0900

    ABC390 E

--- src/bin/abc390_e.rs
+++ src/bin/abc390_e.rs
@@ -19,7 +19,7 @@ fn main() {
     for (i, &(_, a, c)) in vac2.iter().enumerate() {
         for j in 0..(x+1) {
             dp1[i+1][j] = dp1[i+1][j].max(dp1[i][j]);
-            if j+c > x { break; }
+            if j+c > x { continue; }
             if dp1[i][j] < 0 { continue; }
             dp1[i+1][j+c] = dp1[i+1][j+c].max(dp1[i][j] + a);
         } 
@@ -37,7 +37,7 @@ fn main() {
     for (i, &(_, a, c)) in vac2.iter().enumerate() {
         for j in 0..(x+1) {
             dp2[i+1][j] = dp2[i+1][j].max(dp2[i][j]);
-            if j+c > x { break; }
+            if j+c > x { continue; }
             if dp2[i][j] < 0 { continue; }
             dp2[i+1][j+c] = dp2[i+1][j+c].max(dp2[i][j] + a);
         } 
@@ -55,59 +55,60 @@ fn main() {
     for (i, &(_, a, c)) in vac2.iter().enumerate() {
         for j in 0..(x+1) {
             dp3[i+1][j] = dp3[i+1][j].max(dp3[i][j]);
-            if j+c > x { break; }
+            if j+c > x { continue; }
             if dp3[i][j] < 0 { continue; }
             dp3[i+1][j+c] = dp3[i+1][j+c].max(dp3[i][j] + a);
         } 
     }
 
-    // println!("dp1: {:?}", dp1[dp1.len()-1]);
-    // println!("dp2: {:?}", dp2[dp2.len()-1]);
-    // println!("dp3: {:?}", dp3[dp3.len()-1]);
+    // println!("dp1: {:?}", dp1.last().unwrap());
+    // println!("dp2: {:?}", dp2.last().unwrap());
+    // println!("dp3: {:?}", dp3.last().unwrap());
+
+    let mut tmp_max = 0;
+    let mut max_dp1: Vec<isize> = Vec::new();
+    for &vi in dp1[dp1.len()-1].iter() {
+        tmp_max = tmp_max.max(vi);
+        max_dp1.push(tmp_max);
+    }
+
+    let mut tmp_max = 0;
+    let mut max_dp2: Vec<isize> = Vec::new();
+    for &vi in dp2[dp2.len()-1].iter() {
+        tmp_max = tmp_max.max(vi);
+        max_dp2.push(tmp_max);
+    }
+
+    let mut tmp_max = 0;
+    let mut max_dp3: Vec<isize> = Vec::new();
+    for &vi in dp3[dp3.len()-1].iter() {
+        tmp_max = tmp_max.max(vi);
+        max_dp3.push(tmp_max);
+    }
+
+    // println!("max_dp1: {:?}", max_dp1);
+    // println!("max_dp2: {:?}", max_dp2);
+    // println!("max_dp3: {:?}", max_dp3);
 
     let (mut m1, mut m2, mut m3) = (0, 0, 0);
     let (mut x1, mut x2, mut x3) = (0, 0, 0);
-    let mut end_flg = false;
-    while x1 + x2 + x3 < x && !end_flg {
-        if m1 <= m2 && m1 < m3 {
-            for xi in (x1+1)..=x {
-                if dp1[dp1.len()-1][xi] > 0 {
-                    m1 = dp1[dp1.len()-1][xi];
-                    x1 = xi;
-                    break;
-                }
-                if xi == x {
-                    end_flg = true;
-                }
-            }
-        } else if m2 <= m1 && m2 < m3 {
-            for xi in (x2+1)..=x {
-                if dp2[dp2.len()-1][xi] > 0 {
-                    m2 = dp2[dp2.len()-1][xi];
-                    x2 = xi;
-                    break;
-                }
-                if xi == x {
-                    end_flg = true;
-                }
-            }
+    let mut ans: isize = 0;
+    while x1 + x2 + x3 < x {
+        // println!("{} {} {} {}", m1, m2, m3, ans);
+        if m1 <= m2 && m1 <= m3 {
+            x1 += 1;
+            m1 = max_dp1[x1];
+        } else if m2 <= m1 && m2 <= m3 {
+            x2 += 1;
+            m2 = max_dp2[x2];
         } else {
-            for xi in (x3+1)..=x {
-                if dp3[dp3.len()-1][xi] > 0 {
-                    m3 = dp3[dp3.len()-1][xi];
-                    x3 = xi;
-                    break;
-                }
-                if xi == x {
-                    end_flg = true;
-                }
-            }
+            x3 += 1;
+            m3 = max_dp3[x3];
         }
+        ans = ans.max(m1.min(m2).min(m3));
 
-        // println!("m: {:?}, x: {:?}", (m1, m2, m3), (x1, x2, x3));
-        if x1 + x2 + x3 >= x { break; }
     }
 
-    println!("{}", m1.min(m2).min(m3));
+    println!("{}", ans);
 
 }
